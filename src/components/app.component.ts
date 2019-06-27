a9a23c9ec7fc6d4c1f8fd63efc687da96d40c96f
Marius Bongarts <mb709027@fh-muenster.de>
2019-06-27 22:26:11 +0200

    Added TerserPlugin to extract comments of licenses

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -2,13 +2,13 @@ import { css, customElement, html, LitElement, property, unsafeCSS } from 'lit-e
 
 const componentCSS = require('./app.component.scss');
 
-@customElement('app-starter')
+@customElement('marius-button')
 class AppComponent extends LitElement {
 
   static styles = css`${unsafeCSS(componentCSS)}`;
 
   @property()
-  title = 'This title can be overwritten in the custom element by the attribute "title"';
+  title = 'Example button';
 
   emit() {
     console.log('Button clicked');
@@ -21,13 +21,10 @@ class AppComponent extends LitElement {
 
   render() {
     return html`
-          <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
-            <div class="card-header">Web Component</div>
-            <div class="card-body">
-              <h5 class="card-title">${this.title}</h5>
-              <button @click=${() => this.emit()} class="btn btn-primary">Emit Event 'buttonClick'</button>
-            </div>
-          </div>`;
+          <button @click=${()=> this.emit()}>
+          ${this.title}
+          </button>
+`;
   }
 
 }
